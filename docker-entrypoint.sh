39b8c277ea131135b62b035be7e78281babb08e7
a154james <37380983+a154james@users.noreply.github.com>
2026-05-07 20:15:28 -0400

    Add files via upload

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Run Prisma db push to ensure SQLite schema is created on the persistent volume
 echo "Initializing database..."
-npx -y prisma db push
+npx -y prisma@5 db push
 
 # Start the standalone Next.js server
 echo "Starting Next.js server..."
